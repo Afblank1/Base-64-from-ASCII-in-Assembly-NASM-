b624edcbb424bf26ba079a73280d69dbaf552157
Aidan <afbryar@wpi.edu>
2026-02-08 00:43:50 -0500

    Completed the padding logic. Program is now complete!

--- base64.asm
+++ base64.asm
@@ -45,27 +45,27 @@ _start:
 	mov rbx, rax
 	and rbx, 0x3F ; High order 6 bit mask: [0011 1111] to extract first 6 bits
 	movzx r8, byte [base64_table + rbx] ; Get the nth byte from our base64 table by going to the byte indicated by rbx decimal value
-	mov [rdi + 3], r8l ; Move the lowest bit in r8 into the 4th spot in memory present in rdi
+	mov [rdi + 3], r8b ; Move the lowest bit in r8 into the 4th spot in memory present in rdi
 
 	shr rax, 6
 	mov rbx, rax
 	and rbx, 0x3F
 	movzx r8, byte [base64_table + rbx]
-	mov [rdi + 2], r8l
+	mov [rdi + 2], r8b
 	
 
 	shr rax, 6
 	mov rbx, rax
 	and rbx, 0x3F
 	movzx r8, byte [base64_table + rbx]
-	mov [rdi + 1], r8l
+	mov [rdi + 1], r8b
 
 
 	shr rax, 6
 	mov rbx, rax
 	and rbx, 0x3F
 	movzx r8, byte [base64_table + rbx]
-	mov [rdi], r8l
+	mov [rdi], r8b
 
 	add rsi, 3 ; Adds 3 to the pointer that 
 	add rdi, 4 ; Adds 4 to the pointer to the output allowing us to fill the next 4 characters. For each 3 bytes (3 chars) we get 4 base64 characters  
@@ -76,9 +76,73 @@ _start:
 .pad:
 	cmp rcx, 0
 	je .print ; Prints right away if no padding is needed
+  
+	xor rax, rax
+	cmp rcx, 1
+	je .one_byte ; If a 1 bit length is left in the input we need to do different shifts 
+	
+	mov ah, [rsi]
+	mov al, [rsi + 1]
+	shl rax, 8
+
+
+	mov rbx, rax
+	shr rbx, 6
+	and rbx, 0x3F
+	movzx r8, byte [base64_table + rbx]
+	mov [rdi + 2], r8b
+  
+	mov rbx, rax
+	shr rbx, 12
+	and rbx, 0x3F
+	movzx r8, byte [base64_table + rbx]
+	mov [rdi + 1], r8b
+
+	mov rbx, rax
+	shr rbx, 18
+	and rbx, 0x3F
+	movzx r8, byte [base64_table + rbx]
+	mov [rdi], r8b
+	
+	mov byte [rdi + 3], "=" ; Add "=" to pad, we only need one since we can get 3 base64 characters from 2 bytes 
+  add rdi, 4
+	jmp .print
+
+.one_byte:
+	
+	mov al, [rsi]
+	shl rax, 16
+
+	mov rbx, rax
+	shr rbx, 12
+	and rbx, 0x3F
+	movzx r8, byte [base64_table + rbx]
+	mov [rdi + 1], r8b
+
+	mov rbx, rax
+	shr rbx, 18
+	and rbx, 0x3F
+	movzx r8, byte [base64_table + rbx]
+	mov [rdi], r8b
+
+	mov byte [rdi + 2], '='
+  mov byte [rdi + 3], '='
+	add rdi, 4
+
+.print:
+	
+	mov byte [rdi], 10
+	inc rdi
+	mov rdx, rdi
+	sub rdx, output_buffer ; Calculate length for syscall
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, output_buffer
+	syscall 
 
 .exit:
 	mov rax, 60
 	xor rdi, rdi  
 	syscall 
-
+	
